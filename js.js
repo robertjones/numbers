3893662dc2147d1aa8615873fd908f15e29c432e
Rob Jones <robertjones@users.noreply.github.com>
2019-01-13 14:48:09 +0000

    4 option

--- js.js
+++ js.js
@@ -6,11 +6,12 @@ var cVis;
 function rand (n) { return Math.ceil(Math.random()*n); };
 
 function refresh () {
-	currentNum = rand(3);
-	cVis = [currentNum >= 1 ? 1 : 0, currentNum >= 2 ? 1 : 0, currentNum >=3 ? 1 : 0];
+	currentNum = rand(4);
+	cVis = [currentNum >= 1 ? 1 : 0, currentNum >= 2 ? 1 : 0, currentNum >=3 ? 1 : 0, currentNum >=4 ? 1 : 0];
 	document.querySelector('#c1').style.opacity = cVis[0]; 
 	document.querySelector('#c2').style.opacity = cVis[1]; 
 	document.querySelector('#c3').style.opacity = cVis[2];
+document.querySelector('#c4').style.opacity = cVis[3];
 };
 
 function displayMsg (msg) {
